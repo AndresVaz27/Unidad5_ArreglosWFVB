be772ee3e0057ec91b982e7af11b506729abc460
Andres <andresvazquezo1@hotmail.com>
2023-03-14 17:29:20 -0600

    Add project files.

---
+++ Unidad5_ArreglosWFVB/Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSumarFilas = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnSumarColumnas = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnSumarFilas
+        '
+        Me.btnSumarFilas.Location = New System.Drawing.Point(118, 52)
+        Me.btnSumarFilas.Name = "btnSumarFilas"
+        Me.btnSumarFilas.Size = New System.Drawing.Size(102, 46)
+        Me.btnSumarFilas.TabIndex = 0
+        Me.btnSumarFilas.Text = "Sumar Filas"
+        Me.btnSumarFilas.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 147)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(776, 280)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'btnSumarColumnas
+        '
+        Me.btnSumarColumnas.Location = New System.Drawing.Point(530, 52)
+        Me.btnSumarColumnas.Name = "btnSumarColumnas"
+        Me.btnSumarColumnas.Size = New System.Drawing.Size(102, 46)
+        Me.btnSumarColumnas.TabIndex = 3
+        Me.btnSumarColumnas.Text = "Sumar Columnas"
+        Me.btnSumarColumnas.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnSumarColumnas)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnSumarFilas)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnSumarFilas As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnSumarColumnas As Button
+End Class
---
+++ Unidad5_ArreglosWFVB/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Dim arreglo(2, 2) As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Inicializar el arreglo con algunos valores
+        arreglo(0, 0) = 1
+        arreglo(0, 1) = 2
+        arreglo(0, 2) = 3
+        arreglo(1, 0) = 4
+        arreglo(1, 1) = 5
+        arreglo(1, 2) = 6
+        arreglo(2, 0) = 7
+        arreglo(2, 1) = 8
+        arreglo(2, 2) = 9
+
+        ' Enlazar el arreglo con la cuadrícula de datos
+        DataGridView1.DataSource = arreglo
+    End Sub
+    Private Sub btnSumarFilas_Click(sender As Object, e As EventArgs) Handles btnSumarFilas.Click
+        ' Sumar los valores en cada fila del arreglo y mostrar el resultado en un mensaje
+        Dim sumaFilas(2) As Integer
+        For fila As Integer = 0 To 2
+            For columna As Integer = 0 To 2
+                sumaFilas(fila) += arreglo(fila, columna)
+            Next
+        Next
+        MessageBox.Show("La suma de las filas es: " & String.Join(", ", sumaFilas))
+    End Sub
+
+    Private Sub btnSumarColumnas_Click(sender As Object, e As EventArgs) Handles btnSumarColumnas.Click
+        ' Sumar los valores en cada columna del arreglo y mostrar el resultado en un mensaje
+        Dim sumaColumnas(2) As Integer
+        For columna As Integer = 0 To 2
+            For fila As Integer = 0 To 2
+                sumaColumnas(columna) += arreglo(fila, columna)
+            Next
+        Next
+        MessageBox.Show("La suma de las columnas es: " & String.Join(", ", sumaColumnas))
+    End Sub
+End Class
